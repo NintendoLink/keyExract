cd57a3de326eede3779d64c6a64a33593e05f02b
qhl1139 <18705192873@163.com>
2018-04-23 17:04:31 +0800

    fix bug

--- test/Util/DictTest.py
+++
@@ -1,5 +0,0 @@
-# python 3.6.4
-# encoding: utf-8
-from gensim import corpora
-Dict=corpora.Dictionary.load(fname='../Dict2')
-print(Dict)
---
+++ test/readdict.py
@@ -0,0 +1,25 @@
+# python 3.6.4
+# encoding: utf-8
+import pickle
+from gensim import corpora
+import jieba
+import jieba.posseg as pseg
+DICT_DIR='IDF_DICT2'
+IDF_DIR='DICT2'
+CORPUS_DIR='corpus'
+IDF={}
+DICT=corpora.Dictionary.load('DICT2')
+
+# print(DICT.token2id)
+
+# for key in DICT.token2id:
+#     print(key)
+#     print(DICT.token2id[key])
+
+with open(DICT_DIR,'rb') as f:
+    IDF=pickle.load(f)
+
+
+for key in IDF:
+    # print(key)
+    print(IDF[key])
